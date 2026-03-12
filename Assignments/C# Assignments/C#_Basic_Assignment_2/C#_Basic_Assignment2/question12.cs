c4557c06a52d44691e3e5b38d6b405866d0a2d50
Teja Saladi <143919085+Teja-dev-lang@users.noreply.github.com>
2026-03-12 20:56:46 +0530

    commit_C#_AS2

---
+++ Assignments/C# Assignments/C#_Basic_Assignment_2/C#_Basic_Assignment2/question12.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Basic_Assignment2
+{
+    internal class question12
+    {
+        public static void Main()
+        {
+            //12.Write a program to print the numbers divisible by 7 between 200 and 300.
+
+            for(int i = 200; i<=300; i++)
+            {
+                if (i%7 == 0)
+                {
+                    Console.Write(i + ",");
+                }
+            }
+        }
+    }
+}
---
+++ Assignments/C# Assignments/C#_Basic_Assignment_2/C#_Basic_Assignment2/question2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace C__Basic_Assignment2
+{
+    internal class question2
+    {
+        //	Write a program in C# to accept the name of the user as a command line argument and greet the user as:
+        //“Hi! username
+        //Welcome to the world of C#”
+        public static void Main()
+        {
+            Console.WriteLine("Enter Your Name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hi! {name}\n" +
+                $"Welcome to the world of C#");
+        }
+
+    }
+}
